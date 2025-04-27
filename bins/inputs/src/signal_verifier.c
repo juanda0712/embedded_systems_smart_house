0748ac2278edec329062ec9917611b0dff85ea59
Duesan <alejandre030902@gmail.com>
2025-04-27 16:53:25 -0600

    update

--- bins/inputs/src/signal_verifier.c
+++ bins/inputs/src/signal_verifier.c
@@ -2,15 +2,13 @@
 #include <gpiod.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <poll.h>
 
 #define CONSUMER "gpio-input-monitor"
 
 int main(int argc, char **argv) {
     if (argc != 2) {
         printf("Uso: %s <numero_de_pin>\n", argv[0]);
-        return 1;
+        return 2;
     }
 
     int pin = atoi(argv[1]);
@@ -19,44 +17,37 @@ int main(int argc, char **argv) {
     struct gpiod_chip *chip = gpiod_chip_open_by_name(chipname);
     if (!chip) {
         perror("Error al abrir el chip GPIO");
-        return 1;
+        return 2;
     }
 
     struct gpiod_line *line = gpiod_chip_get_line(chip, pin);
     if (!line) {
         perror("Error al obtener la línea GPIO");
         gpiod_chip_close(chip);
-        return 1;
+        return 2;
     }
 
-    if (gpiod_line_request_both_edges_events(line, CONSUMER) < 0) {
-        perror("Error al solicitar eventos en el pin");
+    if (gpiod_line_request_input(line, CONSUMER) < 0) {
+        perror("Error al solicitar la línea como entrada");
         gpiod_chip_close(chip);
-        return 1;
+        return 2;
     }
 
-    struct pollfd pfd;
-    pfd.fd = gpiod_line_event_get_fd(line);
-    pfd.events = POLLIN;
+    int value = gpiod_line_get_value(line);
+    if (value < 0) {
+        perror("Error al leer el valor del pin");
+        gpiod_chip_close(chip);
+        return 2;
+    }
 
-    printf("Esperando eventos en el pin %d...\n", pin);
+    gpiod_chip_close(chip);
 
-    while (1) {
-        int ret = poll(&pfd, 1, -1);
-        if (ret > 0 && (pfd.revents & POLLIN)) {
-            struct gpiod_line_event event;
-            if (gpiod_line_event_read(line, &event) == 0) {
-                if (event.event_type == GPIOD_LINE_EVENT_RISING_EDGE) {
-                    gpiod_chip_close(chip);
-                    return 0
-                    printf("⚡ Evento Rising en el pin %d\n", pin);
-                } else if (event.event_type == GPIOD_LINE_EVENT_FALLING_EDGE) {
-                    gpiod_chip_close(chip);
-                    printf("🔻 Evento Falling en el pin %d\n", pin);
-                    return 1
-                }
-            }
-        }
+    if (value == 1) {
+        printf("⚡ Pin %d está en ALTO (HIGH)\n", pin);
+        return 0;
+    } else {
+        printf("🔻 Pin %d está en BAJO (LOW)\n", pin);
+        return 1;
     }
 }
 
