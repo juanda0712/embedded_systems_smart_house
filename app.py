96831b8e2e33ad7be9a3487421e6de8c0621fadd
juanda0712 <juanrm071299@gmail.com>
2025-04-27 16:55:41 -0600

    connection #1

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request
+import subprocess
+
 
 app = Flask(__name__)
 
@@ -7,21 +9,53 @@ estado = {
     "puertas": [False, False, False, False]
 }
 
+def encender_luz(index):
+    subprocess.run(["bins/outputs/bin/signal_sender", str(index), "on"])
+
+def apagar_luz(index):
+    subprocess.run(["bins/outputs/bin/signal_sender", str(index), "off"])
+
+def leer_puerta(index):
+    resultado = subprocess.run(["bins/inputs/bin/signal_verifier", str(index)])
+    estado = resultado.returncode
+    return estado  # 0 = cerrada, 1 = abierta
+
+
+def tomar_foto():
+    subprocess.run(["bins/photo/bin/photo"])
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/api/estado', methods=["GET", "POST"])
 def obtener_estado():
-    global estado
     if request.method == "POST":
-        datos = request.json
-        estado = datos
-    return jsonify(estado)
+        data = request.json
+        luces = data["luces"]
+        for i, valor in enumerate(luces):
+            if valor:
+                encender_luz(i)
+            else:
+                apagar_luz(i)
+    luces = []  
+    puertas = []
+    for i in range(4):  
+        estado_puerta = leer_puerta(i)
+        puertas.append(True if estado_puerta == 1 else False)
+
+    return jsonify({
+        "luces": luces,  
+        "puertas": puertas
+    })
+
 
 @app.route('/api/camara')
 def camara():
-    return app.send_static_file('picture.jpeg')
+    tomar_foto()
+    return app.send_static_file('jardin.jpg')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
