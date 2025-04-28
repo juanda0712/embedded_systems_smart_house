c4a09894f0ed9b1c22f2895adde146234e754d86
juanda0712 <juanrm071299@gmail.com>
2025-04-27 20:26:23 -0600

    Correction door states and ligths state

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, jsonify, request
 import subprocess
 
-
 app = Flask(__name__)
 
+# Estado SIMULADO en memoria
 estado = {
-    "luces": [False, False, False, False, False],
-    "puertas": [False, False, False, False]
+    "luces": [False, False, False, False, False],  # 5 luces
 }
 
 def encender_luz(index):
@@ -17,45 +16,41 @@ def apagar_luz(index):
 
 def leer_puerta(index):
     resultado = subprocess.run(["bins/inputs/bin/signal_verifier", str(index)])
-    estado = resultado.returncode
-    return estado  # 0 = cerrada, 1 = abierta
-
+    estado_puerta = resultado.returncode
+    return estado_puerta  # 0=cerrada, 1=abierta
 
 def tomar_foto():
     subprocess.run(["bins/photo/bin/photo"])
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/estado', methods=["GET", "POST"])
-def obtener_estado():
+@app.route('/api/luces', methods=["GET", "POST"])
+def luces():
+    global estado
     if request.method == "POST":
         data = request.json
-        luces = data["luces"]
-        for i, valor in enumerate(luces):
+        estado["luces"] = data["luces"]
+        for i, valor in enumerate(estado["luces"]):
             if valor:
                 encender_luz(i)
             else:
                 apagar_luz(i)
-    luces = []  
-    puertas = []
-    for i in range(4):  
-        estado_puerta = leer_puerta(i)
-        puertas.append(True if estado_puerta == 1 else False)
+    return jsonify(estado["luces"])
 
-    return jsonify({
-        "luces": luces,  
-        "puertas": puertas
-    })
-
+@app.route('/api/puertas', methods=["GET"])
+def puertas():
+    puertas_estado = []
+    for i in range(4):  # 4 puertas
+        puerta_abierta = leer_puerta(i)
+        puertas_estado.append(True if puerta_abierta == 1 else False)
+    return jsonify(puertas_estado)
 
 @app.route('/api/camara')
 def camara():
     tomar_foto()
-    return app.send_static_file('jardin.jpg')
+    return app.send_static_file('picture.jpeg')
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
