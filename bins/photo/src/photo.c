503fe97161893970c862622cfd6b741f2fed9a8c
Duesan <alejandre030902@gmail.com>
2025-04-29 02:00:43 -0600

    Se actualizo el metodo de compilacion para que utilizace CMake

--- bins/photo/src/photo.c
+++ bins/photo/src/photo.c
@@ -1,14 +1,14 @@
+#include "photo.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    const char *output_file = "/usr/share/mi-servidor-flask/static/picture.jpg";  // Foto guardada en tmp
-    const char *device = "/dev/video0";          // Dispositivo cámara
+int photo() {
+    const char *device = "/dev/video0";
+    const char *output_path = "/usr/share/mi-servidor-flask/static/picture.jpg";
     const char *command_template = "fswebcam -d %s --jpeg 95 -r 640x480 %s";
-
     char command[256];
-    snprintf(command, sizeof(command), command_template, device, output_file);
 
+    snprintf(command, sizeof(command), command_template, device, output_path);
     printf("Ejecutando: %s\n", command);
 
     int ret = system(command);
@@ -17,8 +17,20 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    printf("Foto capturada en %s\n", output_file);
-
+    printf("Foto capturada en %s\n", output_path);
     return EXIT_SUCCESS;
+}
+
+int main() {
+    printf("Iniciando la captura de foto...\n");
+
+    int result = photo();
+    if (result == EXIT_SUCCESS) {
+        printf("Foto tomada con éxito.\n");
+    } else {
+        printf("Error al tomar la foto.\n");
+    }
+
+    return result;
 }
 
